1611f7740421ada65b535bc037d371a6905d9115
JiangkuoWang <3082899400@qq.com>
2025-02-22 14:47:30 +0800

    update

---
+++ CODE/eval_scripts/math/run.sh
@@ -0,0 +1,11 @@
+PROJECT_PATH=
+MODEL_PATH=
+
+python $PROJECT_PATH/scripts/mgsm_test.py \
+    --model_path $MODEL_PATH \
+    --streategy Parallel \
+    --batch_size 32 \
+    --lang_only Bengali Thai Swahili Japanese Chinese German French Russian Spanish English
+
+
+    
